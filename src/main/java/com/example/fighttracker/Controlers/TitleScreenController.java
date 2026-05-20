5c9b9b4bfaa70541138d43f59614e81566286e0c
kuba852cz <kubicek@spsejecna.cz>
2026-05-20 15:25:09 +0200

    added commands to TitleScreenController.javaScreenControler

--- src/main/java/com/example/fighttracker/Controlers/TitleScreenController.java
+++ src/main/java/com/example/fighttracker/Controlers/TitleScreenController.java
@@ -1,14 +1,46 @@
 package com.example.fighttracker.Controlers;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class TitleScreenController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onRankingButtonClick(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/fighttracker/RankingScreen.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+
+        Scene scene = new Scene(root, 1024, 576);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    protected void onSimulationButtonClick(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/fighttracker/SimulationScreen.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+
+        Scene scene = new Scene(root, 1024, 576);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    protected void onSettingsButtonClick() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/fighttracker/SettingsScreen.fxml"));
+    }
+
+    protected void onExitButtonClick() {
+        Platform.exit();
     }
 }
