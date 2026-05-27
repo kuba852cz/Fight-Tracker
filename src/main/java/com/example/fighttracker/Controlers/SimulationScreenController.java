db729780e1f878b2d55d854b16a650e77a70b1bc
kuba852cz <kubicek@spsejecna.cz>
2026-05-27 16:22:34 +0200

    Did round randomizer

--- src/main/java/com/example/fighttracker/Controlers/SimulationScreenController.java
+++ src/main/java/com/example/fighttracker/Controlers/SimulationScreenController.java
@@ -6,6 +6,7 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -34,7 +35,7 @@ public class SimulationScreenController {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/fighttracker/TitleScreen.fxml"));
         Parent root = loader.load();
 
-        Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
         stage.setMaximized(false);
         stage.setTitle("Fight Tracker");
@@ -43,7 +44,51 @@ public class SimulationScreenController {
         stage.show();
     }
 
-    public void fightSimulation(Fighter fighterA, Fighter fighterB){
+    public String typeOfFinish(Fighter fighterA, Fighter fighterB){
+        double koRatioA = (double) fighterA.getKo() /fighterA.getWins();
+        double koSubmissionA = (double) fighterA.getSubmissions() /fighterA.getWins();
+        double koDeccisionA = (double) fighterA.getDecisions() /fighterA.getWins();
+        double koRatioB = (double) fighterB.getKo() /fighterB.getWins();
+        double koSubmissionB = (double) fighterB.getSubmissions() /fighterB.getWins();
+        double koDeccisionB = (double) fighterB.getDecisions() /fighterB.getWins();
+
+        String finisType = "";
+        int deccisionRandom = new Random().nextInt(0,2);
+
+        int round = finishedAtRound(fighterA, fighterB);
+
+        if (round == 5 && deccisionRandom == 1){
+            finisType = "Deccision";
+        } else if (round == 3 && !fighterA.getRank().equals("C") || !fighterB.getRank().equals("C") && deccisionRandom ==1) {
+            finisType = "Deccision";
+        }else {
+
+
+
+
+
+        }
+
+
+
+        return finisType;
+    }
+
+    public int finishedAtRound(Fighter fighterA, Fighter fighterB){
+
+        int maxRound;
+        if (fighterA.getRank().equals("C") || fighterB.getRank().equals("C")){
+            maxRound = 5;
+        } else {
+            maxRound = 3;
+        }
+
+        return new Random().nextInt(1,maxRound+1);
+    }
+
+    public String fightSimulation(Fighter fighterA, Fighter fighterB){
+
+        String nameOfWinner = "";
         int totalFightsA = (fighterA.getWins() + fighterA.getLosses() + fighterA.getDraws());
         int totalFightsB = (fighterB.getWins() + fighterB.getLosses() + fighterB.getDraws());
 
@@ -71,16 +116,14 @@ public class SimulationScreenController {
 
 
         if (winRandom > (winRatioA + drawMargin)){
-            //B vyhra
+            nameOfWinner = fighterB.getName();
         } else if (winRandom < (winRatioA - drawMargin)) {
-            // A vyhra
+            nameOfWinner = fighterA.getName();
         } else  {
-            //remiza
+            nameOfWinner = "Draw";
         }
 
-
-
-
+        return nameOfWinner;
     }
 
 }
